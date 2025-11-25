6c33e488b910d7cbb0c97a9ccc26709a663eaae6
Often Fred <oftenfred.ict@gmail.com>
2025-11-25 21:00:44 +0300

    Fix Dockerfile to use Render PORT environment variable

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+# Substitute PORT in Apache config
+envsubst '${PORT}' < /etc/apache2/sites-available/000-default.conf > /tmp/apache-config.conf
+mv /tmp/apache-config.conf /etc/apache2/sites-available/000-default.conf
+
+# Update Apache to listen on PORT
+echo "Listen ${PORT:-80}" > /etc/apache2/ports.conf
+
 # Wait for database to be ready (if needed)
 # You can add database connection check here if needed
 
